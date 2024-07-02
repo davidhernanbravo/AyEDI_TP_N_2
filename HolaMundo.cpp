9139fdfca3175f7d87bacd39fd59437450333448
davidhernanbravo <bdavidhernan@gmail.com>
2024-07-02 06:20:13 -0300

    TP N°2 de AyED I

---
+++ HolaMundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Incluye biblioteca de E/S.
+
+using namespace std; // Indica que va a usar Espacio de Nombres (std).
+
+int main() // Función  Principal.
+    {
+        cout << "Hola Mundo!" << endl; // Muestra cadena literal "Hola Mundo!" y hace un salto de línea.
+
+        return 0; // Final de ejecución.
+    }
